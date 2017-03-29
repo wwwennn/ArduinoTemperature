daebba29e4211a20f609ca30c3e5cf2846ad7df2
lingjiagu <lingjia@seas.upenn.edu>
2017-03-29 13:56:04 -0400

    from March 28 recitation

---
+++ index.js
@@ -0,0 +1,26 @@
+Pebble.addEventListener("appmessage", function(e) {
+	sendToServer(); }
+);
+
+function sendToServer() {
+	var req = new XMLHttpRequest();
+	var ipAddress = "158.130.217.102"; // Hard coded IP address
+	var port = "3000"; // Same port specified as argument to server 
+	var url = "http://" + ipAddress + ":" + port + "/";
+	var method = "GET";
+	var async = true;
+	
+	req.onload = function(e) {
+		// see what came back
+		var msg = "no response";
+		var response = JSON.parse(req.responseText); if (response) {
+			if (response.name) {
+				msg = response.name;
+			}else msg = "noname"; 
+		}
+		// sends message back to pebble 
+		  Pebble.sendAppMessage({ "0": msg });
+	};
+	  req.open(method, url, async); 
+	  req.send(null);
+}
